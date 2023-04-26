e5c49c1406e0a4cc2feecacef434522c41661c6f
kgb <kester4000@gmail.com>
2023-04-26 08:53:25 -0400

    Added icons to footer.

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,21 @@ export default function Home() {
   return (
     <>
     <div className="container">
-      <header><h1>Prince Shellz</h1></header>
-      <div className="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/bFIKVSkJHOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+      <header><h1>Marrico</h1></header>
+      <div className="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/JcqyTVJopr0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
       <div className="info">
-        Contact
+        Contact Marrico
       </div>
     </div>
 
     <footer className="footer">
       Prince Shellz &copy;{new Date().getFullYear()}
-      <img src="images/instagram.png" />
+      <div>
+        <img src="images/instagram.png" />
+        <img src="images/spotify.png" />
+        <img src="images/itunes.png" />
+      </div>
+      
     </footer>
     </>
   )
