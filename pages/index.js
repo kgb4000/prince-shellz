2f4998234c3ae9504244134c295025326567536b
kgb <kester4000@gmail.com>
2023-05-01 23:13:57 -0400

    Added links to icons.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-
-
-
 export default function Home() {
   return (
     <>
+    {/* <video autoplay muted loop id="myVideo">
+      <source src="/video/maricco.mov" type="video/mov"/>
+    </video> */}
     <div className="container">
-      <header><h1>Marrico</h1></header>
-      <div className="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/JcqyTVJopr0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+      <header><h1>Marric0</h1></header>
+      <div className="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/JcqyTVJopr0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe>
+      </div>
       <div className="info">
-        Contact Marrico
+        Contact Marric0
       </div>
     </div>
 
     <footer className="footer">
-      Prince Shellz &copy;{new Date().getFullYear()}
+      Marric0 &copy;{new Date().getFullYear()}
       <div>
-        <img src="images/instagram.png" />
-        <img src="images/spotify.png" />
-        <img src="images/itunes.png" />
+        <a href="https://www.instagram.com/marric0/?hl=en" target="_blank"><img src="images/instagram.png" alt="Instagram"/></a>
+        <a href="https://open.spotify.com/artist/0fXRvGwnIicMcZpAiqtkBz" target="_blank"><img src="images/spotify.png" alt="Spotify"/></a>
+        <a href="https://music.apple.com/us/artist/marrico/375256925" target="_blank"><img src="images/itunes.png" alt="Apple music"/></a>
       </div>
       
     </footer>
